96f9f165b1a897e50650bc7b4f7109ca1c6d7f6f
vladimir858 <standardwebsites@gmail.com>
2026-07-07 11:22:21 +0300

    FSD

---
+++ src/entities/todo/model/TasksContext.jsx
@@ -0,0 +1,54 @@
+import { createContext } from 'react'
+import useTasks from './useTasks'
+import useIncompleteTaskScroll from './useIncompleteTaskScroll'
+
+export const TasksContext = createContext({})
+
+export const TasksProvider = (props) => {
+  const { children } = props
+
+  const {
+    tasks,
+    filteredTasks,
+    deleteTask,
+    deleteAllTasks,
+    toggleTaskComplete,
+    newTaskTitle,
+    setNewTaskTitle,
+    searchQuery,
+    setSearchQuery,
+    newTaskInputRef,
+    addTask,
+    disappearingTaskId,
+    appearingTaskId,
+  } = useTasks()
+
+  const {
+    firstIncompleteTaskRef,
+    firstIncompleteTaskId,
+  } = useIncompleteTaskScroll(tasks)
+
+  return (
+    <TasksContext.Provider
+      value={{
+        tasks,
+        filteredTasks,
+        firstIncompleteTaskRef,
+        firstIncompleteTaskId,
+        deleteTask,
+        deleteAllTasks,
+        toggleTaskComplete,
+        newTaskTitle,
+        setNewTaskTitle,
+        searchQuery,
+        setSearchQuery,
+        newTaskInputRef,
+        addTask,
+        disappearingTaskId,
+        appearingTaskId,
+      }}
+    >
+      {children}
+    </TasksContext.Provider>
+  )
+}
